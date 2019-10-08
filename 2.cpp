a5f6afbb3fad3c5b2f837e4308ec828eb8f1d1da
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-08 12:42:23 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -6,54 +6,52 @@ using namespace std;
 
 int main()
 {
-    int l;
-    cout<<"Vvedite L"<<endl;
-    cin>>l;
-    
-    int n;
-    cout<<"Vvedite N"<<endl;
-    cin>>n;
-    
-    cout<<"Vvedite massiv"<<endl;
-    vector<float> arr(n+1);
-    for (int i=0; i<n; i++)
-    {
-        cin>>arr[i];
-    }
-    arr[n]=-498241;
-    
-    int kolvo=1;
-    int counter=0;
-    int begin=counter;
-    
-    while (counter<arr.size()-10)
-    {
-        if (arr[counter]==arr[counter+1])
-        {
-            kolvo++;
-            counter++;
-        }
-        else 
-        {
-            if (kolvo>l)
-            {
-                for (int i=0; i<counter-begin+1; i++)
-                {
-                    arr.erase(arr.begin()+begin);
-                }
-                counter=counter-kolvo;
-                begin=counter;
-                kolvo=1;
-            }
-            counter++;
-        }
-        
-    }
-    
-    arr.erase(arr.end());
-    
-    copy(arr.begin(), arr.end(), ostream_iterator<float>(cout, " "));
-    
-    return 0;
-    
+	int l;
+	cout << "Vvedite L" << endl;
+	cin >> l;
+
+	int n;
+	cout << "Vvedite N" << endl;
+	cin >> n;
+
+	cout << "Vvedite massiv" << endl;
+	vector<float> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int kolvo = 1;
+	int counter = 0;
+	int begin = counter;
+
+	while (counter < arr.size()-1 )
+	{
+		if (arr[counter] == arr[counter + 1])
+		{
+			kolvo++;
+			counter++;
+		}
+		else
+		{
+			if (kolvo > l)
+			{
+				for (int i = 0; i < counter - begin+1; i++)
+				{
+					arr.erase(arr.begin() + begin);
+				}
+				arr.insert(arr.begin() + begin, 0);
+				counter = counter - kolvo;
+				begin = counter;
+				kolvo = 1;
+			}
+			counter++;
+		}
+
+	}
+
+	copy(arr.begin(), arr.end(), ostream_iterator<float>(cout, " "));
+
+	return 0;
+
 }
